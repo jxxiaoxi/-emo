7d8f0ea5cc3919606228905f25ec313484f4c1a8
liuwei <liuwei@localhost.com>
2017-06-24 19:32:12 +0800

    face

--- -emo/VoiceRecoder/app/src/main/java/com/mj/voicerecoder/FaceDetectActivity.java
+++ -emo/VoiceRecoder/app/src/main/java/com/mj/voicerecoder/FaceDetectActivity.java
@@ -1,24 +1,43 @@
 package com.mj.voicerecoder;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.FaceRequest;
+import com.iflytek.cloud.RequestListener;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
 import com.mj.voicerecoder.base.BaseActivity;
-import com.mj.voicerecoder.util.AppUtils;
+import com.mj.voicerecoder.face.util.FaceUtil;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.UnsupportedEncodingException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,20 +53,59 @@ public class FaceDetectActivity extends BaseActivity {
     TextureView surfaceView;
     @BindView(R.id.bt_register)
     Button btRegister;
-    @BindView(R.id.bt_detect)
+    @BindView(R.id.online_verify)
     Button btDetect;
+    @BindView(R.id.online_authid)
+    EditText onlineAuthid;
+    @BindView(R.id.online_pick)
+    Button onlinePick;
+    @BindView(R.id.online_camera)
+    Button onlineCamera;
+    @BindView(R.id.online_img)
+    ImageView onlineImg;
 
     private Camera mCamera;
     private static final int YUV_1080_SIZE = 3110400;
     private byte[] mImageCallbackBuffer = new byte[YUV_1080_SIZE];
+    private FaceTask mFaceTask;
+    // authid为6-18个字符长度，用于唯一标识用户
+    private String mAuthid = null;
+    // 进度对话框
+    private ProgressDialog mProDialog;
+    // FaceRequest对象，集成了人脸识别的各种功能
+    private FaceRequest mFaceRequest;
+    private final int REQUEST_PICTURE_CHOOSE = 1;
+    private final int REQUEST_CAMERA_IMAGE = 2;
+    // 拍照得到的照片文件
+    private File mPictureFile;
+    private Bitmap mImage = null;
+    private byte[] mImageData = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face);
         ButterKnife.bind(this);
-        surfaceView.setSurfaceTextureListener(new CoustomFaceViewListener());
-        openCamera(1);
+        // surfaceView.setSurfaceTextureListener(new CoustomFaceViewListener());
+        //  openCamera(1);
+
+
+        mProDialog = new ProgressDialog(this);
+        mProDialog.setCancelable(true);
+        mProDialog.setTitle("请稍后");
+
+        mProDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                // cancel进度框时,取消正在进行的操作
+                if (null != mFaceRequest) {
+                    mFaceRequest.cancel();
+                }
+            }
+        });
+
+        mFaceRequest = new FaceRequest(this);
     }
 
     @Override
@@ -55,22 +113,160 @@ public class FaceDetectActivity extends BaseActivity {
         return true;
     }
 
-    @OnClick({R.id.bt_register, R.id.bt_detect})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.bt_register:
-                Log.e("mijie", "bt_register ");
-                takePhoto();
-                break;
-            case R.id.bt_detect:
-                break;
+    private RequestListener mRequestListener = new RequestListener() {
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
         }
-    }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            if (null != mProDialog) {
+                mProDialog.dismiss();
+            }
+
+            try {
+                String result = new String(buffer, "utf-8");
+                Log.d("FaceDemo", result);
+
+                JSONObject object = new JSONObject(result);
+                String type = object.optString("sst");
+                if ("reg".equals(type)) {
+                    register(object);
+                } else if ("verify".equals(type)) {
+                    verify(object);
+                }
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JSONException e) {
+                // TODO: handle exception
+            }
+        }
+
+
+        private void register(JSONObject obj) throws JSONException {
+            int ret = obj.getInt("ret");
+            if (ret != 0) {
+                showTip("注册失败");
+                return;
+            }
+            if ("success".equals(obj.get("rst"))) {
+                showTip("注册成功");
+            } else {
+                showTip("注册失败");
+            }
+        }
+
+
+        private void verify(JSONObject obj) throws JSONException {
+            int ret = obj.getInt("ret");
+            if (ret != 0) {
+                showTip("验证失败");
+                return;
+            }
+            if ("success".equals(obj.get("rst"))) {
+                if (obj.getBoolean("verf")) {
+                    showTip("通过验证，欢迎回来！");
+                } else {
+                    showTip("验证失败，不是同一个人");
+                }
+            } else {
+                showTip("验证失败");
+            }
+        }
+
+        @Override
+        public void onCompleted(SpeechError error) {
+            if (null != mProDialog) {
+                mProDialog.dismiss();
+            }
+
+            if (error != null) {
+                switch (error.getErrorCode()) {
+                    case ErrorCode.MSP_ERROR_ALREADY_EXIST:
+                        showTip("authid已经被注册，请更换后再试");
+                        break;
+
+                    default:
+                        showTip(error.getPlainDescription(true));
+                        break;
+                }
+            }
+        }
+    };
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @OnClick({R.id.online_pick, R.id.online_camera, R.id.bt_register, R.id.online_verify})
+    public void onViewClicked(View view) {
+        int ret = ErrorCode.SUCCESS;
+        switch (view.getId()) {
+            case R.id.online_pick:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(intent, REQUEST_PICTURE_CHOOSE);
+                break;
+            case R.id.online_camera:
+                // 设置相机拍照后照片保存路径
+                mPictureFile = new File(Environment.getExternalStorageDirectory(),
+                        "picture" + System.currentTimeMillis() / 1000 + ".jpg");
+                // 启动拍照,并保存到临时文件
+                Intent mIntent = new Intent();
+                mIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                mIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPictureFile));
+                mIntent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
+                startActivityForResult(mIntent, REQUEST_CAMERA_IMAGE);
+                break;
+            case R.id.bt_register:
+                mAuthid = ((EditText) findViewById(R.id.online_authid)).getText().toString();
+                if (TextUtils.isEmpty(mAuthid)) {
+                    showTip("authid不能为空");
+                    return;
+                }
+
+                if (null != mImageData) {
+                    mProDialog.setMessage("注册中...");
+                    mProDialog.show();
+                    // 设置用户标识，格式为6-18个字符（由字母、数字、下划线组成，不得以数字开头，不能包含空格）。
+                    // 当不设置时，云端将使用用户设备的设备ID来标识终端用户。
+                    mFaceRequest.setParameter(SpeechConstant.AUTH_ID, mAuthid);
+                    mFaceRequest.setParameter(SpeechConstant.WFR_SST, "reg");
+                    ret = mFaceRequest.sendRequest(mImageData, mRequestListener);
+                } else {
+                    showTip("请选择图片后再注册");
+                }
+                break;
+            case R.id.online_verify:
+                mAuthid = ((EditText) findViewById(R.id.online_authid)).getText().toString();
+                if (TextUtils.isEmpty(mAuthid)) {
+                    showTip("authid不能为空");
+                    return;
+                }
+
+                if (null != mImageData) {
+                    mProDialog.setMessage("验证中...");
+                    mProDialog.show();
+                    // 设置用户标识，格式为6-18个字符（由字母、数字、下划线组成，不得以数字开头，不能包含空格）。
+                    // 当不设置时，云端将使用用户设备的设备ID来标识终端用户。
+                    mFaceRequest.setParameter(SpeechConstant.AUTH_ID, mAuthid);
+                    mFaceRequest.setParameter(SpeechConstant.WFR_SST, "verify");
+                    ret = mFaceRequest.sendRequest(mImageData, mRequestListener);
+                } else {
+                    showTip("请选择图片后再验证");
+                }
+                break;
+
+        }
+        if (ErrorCode.SUCCESS != ret) {
+            mProDialog.dismiss();
+            showTip("出现错误：" + ret);
+        }
     }
 
     class CoustomFaceViewListener implements TextureView.SurfaceTextureListener {
@@ -125,7 +321,17 @@ public class FaceDetectActivity extends BaseActivity {
         mCamera.setPreviewCallbackWithBuffer(new Camera.PreviewCallback() {
             @Override
             public void onPreviewFrame(byte[] bytes, Camera camera) {
-
+                if (null != mFaceTask) {
+                    switch (mFaceTask.getStatus()) {
+                        case RUNNING:
+                            return;
+                        case PENDING:
+                            mFaceTask.cancel(false);
+                            break;
+                    }
+                }
+                mFaceTask = new FaceTask(bytes);
+                mFaceTask.execute((Void) null);
 
                 mCamera.addCallbackBuffer(bytes);
             }
@@ -135,8 +341,9 @@ public class FaceDetectActivity extends BaseActivity {
 
     private class FaceTask extends AsyncTask<Void, Void, Void> {
         private byte[] mData;
+
         //构造函数
-        FaceTask(byte[] data){
+        FaceTask(byte[] data) {
             this.mData = data;
         }
 
@@ -146,34 +353,139 @@ public class FaceDetectActivity extends BaseActivity {
         }
     }
 
-    private void takePhoto() {
-        if (mCamera == null)
+//    private void takePhoto() {
+//        if (mCamera == null)
+//            return;
+//
+//        mCamera.takePicture(null, null, new Camera.PictureCallback() {
+//            @Override
+//            public void onPictureTaken(byte[] bytes, Camera camera) {
+//                FileOutputStream outSteam = null;
+//                SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+//                String times = format.format((new Date()));
+//                String path = AppUtils.getAppPath()+times+ ".jpg";
+//                try {
+//
+//                    File file = new File(path);
+//                    Log.e("liuwei","xxxx "+file.getParentFile().exists());
+//                    if (!file.getParentFile().exists()) {
+//                        file.getParentFile().mkdirs();
+//                    }
+//                    outSteam = new FileOutputStream(path);
+//                    outSteam.write(bytes);
+//                    outSteam.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        });
+//
+//    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
             return;
+        }
 
-        mCamera.takePicture(null, null, new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] bytes, Camera camera) {
-                FileOutputStream outSteam = null;
-                SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
-                String times = format.format((new Date()));
-                String path = AppUtils.getAppPath()+times+ ".jpg";
-                try {
-
-                    File file = new File(path);
-                    Log.e("liuwei","xxxx "+file.getParentFile().exists());
-                    if (!file.getParentFile().exists()) {
-                        file.getParentFile().mkdirs();
-                    }
-                    outSteam = new FileOutputStream(path);
-                    outSteam.write(bytes);
-                    outSteam.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+        String fileSrc = null;
+        if (requestCode == REQUEST_PICTURE_CHOOSE) {
+            if ("file".equals(data.getData().getScheme())) {
+                // 有些低版本机型返回的Uri模式为file
+                fileSrc = data.getData().getPath();
+            } else {
+                // Uri模型为content
+                String[] proj = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(data.getData(), proj,
+                        null, null, null);
+                cursor.moveToFirst();
+                int idx = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                fileSrc = cursor.getString(idx);
+                cursor.close();
             }
-        });
+            // 跳转到图片裁剪页面
+            FaceUtil.cropPicture(this, Uri.fromFile(new File(fileSrc)));
+        } else if (requestCode == REQUEST_CAMERA_IMAGE) {
+            if (null == mPictureFile) {
+                showTip("拍照失败，请重试");
+                return;
+            }
 
+            fileSrc = mPictureFile.getAbsolutePath();
+            updateGallery(fileSrc);
+            // 跳转到图片裁剪页面
+            FaceUtil.cropPicture(this, Uri.fromFile(new File(fileSrc)));
+        } else if (requestCode == FaceUtil.REQUEST_CROP_IMAGE) {
+            // 获取返回数据
+            Bitmap bmp = data.getParcelableExtra("data");
+            // 若返回数据不为null，保存至本地，防止裁剪时未能正常保存
+            if (null != bmp) {
+                FaceUtil.saveBitmapToFile(this, bmp);
+            }
+            // 获取图片保存路径
+            fileSrc = FaceUtil.getImagePath(this);
+            // 获取图片的宽和高
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            mImage = BitmapFactory.decodeFile(fileSrc, options);
+
+            // 压缩图片
+            options.inSampleSize = Math.max(1, (int) Math.ceil(Math.max(
+                    (double) options.outWidth / 1024f,
+                    (double) options.outHeight / 1024f)));
+            options.inJustDecodeBounds = false;
+            mImage = BitmapFactory.decodeFile(fileSrc, options);
+
+
+            // 若mImageBitmap为空则图片信息不能正常获取
+            if (null == mImage) {
+                showTip("图片信息无法正常获取！");
+                return;
+            }
+
+            // 部分手机会对图片做旋转，这里检测旋转角度
+            int degree = FaceUtil.readPictureDegree(fileSrc);
+            if (degree != 0) {
+                // 把图片旋转为正的方向
+                mImage = FaceUtil.rotateImage(degree, mImage);
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            //可根据流量及网络状况对图片进行压缩
+            mImage.compress(Bitmap.CompressFormat.JPEG, 80, baos);
+            mImageData = baos.toByteArray();
+
+            ((ImageView) findViewById(R.id.online_img)).setImageBitmap(mImage);
+        }
+
+    }
+
+    private void updateGallery(String filename) {
+        MediaScannerConnection.scanFile(this, new String[]{filename}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void finish() {
+        if (null != mProDialog) {
+            mProDialog.dismiss();
+        }
+        super.finish();
+    }
+
+    private void showTip(final String str) {
+        Toast toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+        toast.setText(str);
+        toast.show();
     }
 
 }
